98d86d926f4cdc53f420ba016ea504d6823bd882
Justin W <justinwei.dev@gmail.com>
2021-04-07 13:30:34 +1200

    added scores that update after each round

--- game.js
+++ game.js
@@ -2,19 +2,34 @@ const choices = ["rock", "paper", "scissors"];
 
 let score = []
 let roundCounter = 0
+let playerScore = 0;
+let computerScore = 0;
 
 const btnChoices = document.querySelectorAll(".choices");
-console.log(btnChoices);
+const playerP = document.querySelector("#player-score");
+const computerP = document.querySelector("#computer-score")
 
 btnChoices.forEach((btnChoice) => {
-    btnChoice.addEventListener("click", (e) => {
-        console.log("yes");
+    btnChoice.addEventListener("click", (e) => { 
         const chosenButton = document.querySelector(`#btn-${e.target.getAttribute('data-choice')}`);
         if (!chosenButton) return;
         let playerChoice = chosenButton.getAttribute('data-choice')
+        roundCounter++;
         playRound(score, playerChoice);
+        console.log(score);
+        console.log(computerScore);
+        updateScores();
+        if (playerScore == 5) { console.log("You win the game!") };
+        if (computerScore == 5) { console.log("Computer won the game!") };
+
     })
 })
+
+function updateScores() {
+    console.log(playerP);
+    playerP.innerHTML = `${playerScore}`;
+    computerP.innerHTML = `${computerScore}`;
+}
 
 function playRound(score, playerChoice) {
     score.push(compareResults(playerChoice,computerPlay()));
@@ -27,11 +42,6 @@ function computerPlay() {
     return computerChoice;
 }
 
-function getUserInput() {
-    let choice = "paper";
-    return choice;
-}
-
 function compareResults(one, two) {
     let p = choices.indexOf(one)+1;
     let c = -(choices.indexOf(two)+1);
@@ -41,34 +51,26 @@ function compareResults(one, two) {
     if (sum == -1 || sum == 2) {
         message = `Computer wins! ${ two } beats ${ one }!`;
         result = 2;
+        computerScore++;
     }
     else if (sum == 1 || sum == -2) {
         message = `Player wins! ${ one } beats ${ two }!`;
         result = 1;
+        playerScore++;
     }
     else if (sum == 0) {
         message = `Tie! You both drew ${ one }.`;
         result = 0;
     }
     else { 
-        console.log("something went wrong");
+        console.log("SOMETHING WENT WRONG");
         result = -1;
     }
     console.log(message);
     return result;
     }
 
-/* function game() {
-    console.log("game start");
-    let score = [];
-    for (i=0; i < 5; i++){
-        playRound(score);
-    }
-    findWinner(score);
-} */
-
-
-function findWinner(scoreList) {
+/* function findWinner(scoreList) {
     let playerScore = 0;
     let computerScore = 0;
     let winner = '';
@@ -88,4 +90,4 @@ function findWinner(scoreList) {
     if (winner == "computer") {
         console.log("Computer won the game!");
     }
-}
+} */
