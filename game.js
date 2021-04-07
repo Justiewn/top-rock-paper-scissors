78d45286decd77571eaae8e2e33ec8c56c7274b9
Justin W <justinwei.dev@gmail.com>
2021-04-07 14:10:28 +1200

    added reset functionality

--- game.js
+++ game.js
@@ -16,16 +16,23 @@ btnChoices.forEach((btnChoice) => {
         const chosenButton = document.querySelector(`#btn-${e.target.getAttribute('data-choice')}`);
         if (!chosenButton) return;
         let playerChoice = chosenButton.getAttribute('data-choice')
-        roundCounter++;
         playRound(score, playerChoice);
-        console.log(score);
-        console.log(computerScore);
         updateScores();
         if (playerScore == 5) { winnerP.textContent = "You win the game!"; };
         if (computerScore == 5) { winnerP.textContent = "Computer won the game!"; };
 
     })
 })
+
+function resetGame() {
+    score = []
+    roundCounter = 0
+    playerScore = 0;
+    computerScore = 0;
+    resultsP.textContent = "-";
+    winnerP.textContent = "-";
+    updateScores();
+}
 
 function updateScores() {
     console.log(playerP);
@@ -34,6 +41,7 @@ function updateScores() {
 }
 
 function playRound(score, playerChoice) {
+    roundCounter++;
     score.push(compareResults(playerChoice,computerPlay()));
     return score;
 }
@@ -71,25 +79,3 @@ function compareResults(one, two) {
     resultsP.textContent = `${message}`;
     return result;
     }
-
-/* function findWinner(scoreList) {
-    let playerScore = 0;
-    let computerScore = 0;
-    let winner = '';
-    for (x = 0; x < scoreList.length; x++) {
-        if (scoreList[x] == 1) { playerScore++;}
-        else if (scoreList[x]  == 2) { computerScore ++;}
-    }
-    if (playerScore == computerScore) { winner = 'tied'; return winner;}
-    else if (playerScore > computerScore) { winner = 'player'; return winner;}
-    winner = 'computer';
-    if (winner == "tied") {
-        console.log("Game resulted in a tie!");
-    }   
-    if (winner == "player") {
-        console.log("You win the game!");
-    }
-    if (winner == "computer") {
-        console.log("Computer won the game!");
-    }
-} */
